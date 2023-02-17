c34ed6d6c8f07677f30b0900385e9a95e49f9f05
banea-radu <100232231+banea-radu@users.noreply.github.com>
2023-02-17 22:17:42 +0200

    import component - find already existing id's (#50)

--- src/app/component/import/import.component.ts
+++ src/app/component/import/import.component.ts
@@ -11,27 +11,48 @@ import { DatabaseService } from 'src/app/service/database.service';
 export class ImportComponent {
 
   // rawData: any;
-  rawData$: any;
+  importData$: any;
+  duplicateIdsArray: number[] = [];
   // data: any;
   startDate = new Date(1263722547*1000);
-  startDayMonthYear = this.startDate.getDate() + '.' + (this.startDate.getMonth() + 1) + '.' + this.startDate.getFullYear()
+  startDayMonthYear = this.startDate.getDate() + '.' + (this.startDate.getMonth() + 1) + '.' + this.startDate.getFullYear();
   endDate = new Date(1675285130*1000);
-  endDayMonthYear = this.endDate.getDate() + '.' + (this.startDate.getMonth() + 1) + '.' + this.endDate.getFullYear()
+  endDayMonthYear = this.endDate.getDate() + '.' + (this.startDate.getMonth() + 1) + '.' + this.endDate.getFullYear();
+  clientIdsFromDatabase: any;
 
-  constructor(private jsonService: JsonService) { }
+  constructor(
+    private jsonService: JsonService,
+    private databaseService: DatabaseService
+  ) { }
 
   ngOnInit() {
-    this.rawData$ = this.jsonService.getJsonFile()
-      .pipe(map((response: any) => {
-      //   const oldArrayOfObjects = response.friends_v2;
-      //   const newArrayOfObjects = [];
-      //   oldArrayOfObjects.forEach((item, i: number = 0) => {
-      //     let id = item.timestamp;
-      //     newArrayOfObjects.push({...oldArrayOfObjects[i++], id: id});
-      //   });
-      //   console.log(newArrayOfObjects);
-        return response.friends_v2;
-      }))
+    this.jsonService.getJsonFile().subscribe((importData: any) => {
+      this.databaseService.getClients().subscribe((clients) => {
+        // const intersection = array1.filter(element => array2.includes(element));
+        importData.friends_v2.forEach((newClient) => {
+          clients.forEach((dbClient) => {
+            if (newClient.timestamp == dbClient.Id) {
+              this.duplicateIdsArray.push(newClient.timestamp);
+            }          
+          });
+        });
+      });
+    });
+  }
+    
+
+
+
+      // .pipe(map((response: any) => {
+      //           //   const oldArrayOfObjects = response.friends_v2;
+      //           //   const newArrayOfObjects = [];
+      //           //   oldArrayOfObjects.forEach((item, i: number = 0) => {
+      //           //     let id = item.timestamp;
+      //           //     newArrayOfObjects.push({...oldArrayOfObjects[i++], id: id});
+      //           //   });
+      //           //   console.log(newArrayOfObjects);
+      //   return response.friends_v2;
+      // }))
       // .pipe(
       //   tap(response => {
       //     response.forEach((item) => {
@@ -58,5 +79,4 @@ export class ImportComponent {
     //     // this.data = data;
     //     console.log(data.friends_v2);
     //   });
-  }
 }
