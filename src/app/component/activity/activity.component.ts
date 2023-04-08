af4b49683f65bc635e71ced94e1079a0519c8b75
banea-radu <100232231+banea-radu@users.noreply.github.com>
2023-04-08 21:57:25 +0300

    activity component (#92)

--- src/app/component/activity/activity.component.ts
+++ src/app/component/activity/activity.component.ts
@@ -8,28 +8,32 @@ import { DatabaseService } from 'src/app/service/database.service';
   styleUrls: ['./activity.component.css']
 })
 export class ActivityComponent {
-  // create Id from current date
-  today: Date = new Date();
-  currentYear: number = this.today.getFullYear();
-  currentMonth: number = this.today.getMonth() + 1;
-  currentMonthString: string = 
-    this.currentMonth < 10
-      ? '0' + this.currentMonth
-      : this.currentMonth.toString();
-  currentWeekDay: number = this.today.getDay();
-  startOfCurrentWeek: number = (this.currentWeekDay == 0 ? -6 : 1)
-    + this.today.getDate() - this.currentWeekDay;
-  startOfCurrentWeekString: string =
-    this.startOfCurrentWeek < 10
-      ? '0' + this.startOfCurrentWeek
-      : this.startOfCurrentWeek.toString();
-  endOfCurrentWeek: number = this.startOfCurrentWeek + 6;
-  endOfCurrentWeekString: string =
-    this.endOfCurrentWeek < 10
-      ? '0' + this.endOfCurrentWeek
-      : this.endOfCurrentWeek.toString();
-  thisWeekId: string = this.currentYear + this.currentMonthString + this.startOfCurrentWeekString + this.endOfCurrentWeekString;
-
+  // create Id from selected date
+  today: Date = new Date("2023-03-30");
+  selectedDate: Date = this.today;
+  firstDayOfWeek: Date;
+  lastDayOfWeek: Date;
+  
+  // selectedYear: number = this.selectedDate.getFullYear();
+  // selectedMonth: number = this.selectedDate.getMonth() + 1;
+  // selectedMonthString: string = 
+  //   this.selectedMonth < 10
+  //     ? '0' + this.selectedMonth
+  //     : this.selectedMonth.toString();
+  // selectedWeekDay: number = this.selectedDate.getDay();
+  // startDateOfWeek: number = + this.selectedDate.getDate() - this.selectedWeekDay + (this.selectedWeekDay == 0 ? -6 : 1);
+  // startDateOfWeekString: string =
+  //   this.startDateOfWeek < 10
+  //     ? '0' + this.startDateOfWeek
+  //     : this.startDateOfWeek.toString();
+  // endDateOfWeek: number = this.startDateOfWeek + 6;
+  // endDateOfWeekString: string =
+  //   this.endDateOfWeek < 10
+  //     ? '0' + this.endDateOfWeek
+  //     : this.endDateOfWeek.toString();
+  // thisWeekId: string = this.selectedYear + this.selectedMonthString + this.startDateOfWeekString + this.endDateOfWeekString;
+  thisWeekId = "2023040309";
+ 
   $activities: Observable<any>;
 
   constructor(
@@ -38,6 +42,18 @@ export class ActivityComponent {
 
   ngOnInit() {
     this.getActivities();
+    this.getFirstAndLastDayOfWeek();
+    console.log(this.firstDayOfWeek + '' + this.lastDayOfWeek);
+  }
+  
+  getFirstAndLastDayOfWeek() {
+    const date = new Date(this.selectedDate);
+    const day = date.getDay(); // get day of week
+    const diff = date.getDate() - day + (day === 0 ? -6 : 1); // day of month - day of week (-6 if Sunday), otherwise +1
+    this.firstDayOfWeek = new Date(date.setDate(diff));
+
+    this.lastDayOfWeek = new Date(this.firstDayOfWeek);
+    this.lastDayOfWeek.setDate(this.lastDayOfWeek.getDate() + 6);
   }
 
   getActivities() {
